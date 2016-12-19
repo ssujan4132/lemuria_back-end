ea55c25c66d0cd436332491e8776125b2d355a65
Sujan Selvanayagam <ssujan4132@gmail.com>
2016-12-19 15:27:56 +0800

    test

--- models/code.js
+++ models/code.js
@@ -1,5 +1,5 @@
 const REGISTER_OK = 1000;
-const REGISTER_OK_MSG = "Registration Successful";
+const REGISTER_OK_MSG = "Registration Successfulss";
 
 const REGISTER_INSERT_ERR = 1001;
 const REGISTER_INSERT_ERR_MSG = "Unable to save registration"
